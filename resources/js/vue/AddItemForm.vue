aa2576dd288b489208477b104c1bf00aeb693518
Roman Kamenivskyi <rk@wwwest.solutions>
2021-03-29 01:00:13 +0300

    implement add item form component with adding new task functionality

---
+++ resources/js/vue/AddItemForm.vue
@@ -0,0 +1,41 @@
+<template>
+    <form class="form" @submit.prevent="onAddNewTask">
+        <input class="input-field" type="text" placeholder="Enter task name" v-model="inputField" />
+        <button>Submit</button>
+    </form>
+</template>
+
+<script>
+    import axios from 'axios';
+    export default {
+        data: () => ({
+            inputField: '',
+        }),
+        methods: {
+            async onAddNewTask() {
+                if (this.inputField.trim()) {
+                    await axios.post('api/item/store', {
+                        item: {
+                            name: this.inputField
+                        }
+                    });
+                    this.inputField = '';
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+    .form {
+        display: flex;
+    }
+    .input-field {
+        width: 100%;
+        padding: 10px 15px;
+    }
+    .input-field:focus {
+        border: 2px solid green;
+        outline: 0;
+    }
+</style>
--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -1,15 +1,22 @@
 <template>
-    <div>
-        Hello {{ name }}
+    <div class="app-container">
+        <AddItemForm />
     </div>
 </template>
 
 <script>
+    import AddItemForm from "./AddItemForm";
     export default {
-        data() {
-            return {
-                name: 'Roma',
-            };
+        components: {
+            AddItemForm
         }
     }
 </script>
+
+<style>
+    .app-container {
+        width: 400px;
+        max-width: 90%;
+        margin: 0 auto;
+    }
+</style>
